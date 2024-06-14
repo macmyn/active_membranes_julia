f23a610eaf2c61ac562614188c776d4a8c06810d
macmyn <21172144+macmyn@users.noreply.github.com>
2024-06-14 17:03:52 +0100

    main code shape functions (not working; error with array types)

--- scripts/active_membrane.jl
+++ scripts/active_membrane.jl
@@ -1,37 +1,56 @@
 using DrWatson
 @quickactivate
 
-using ModelingToolkit, MethodOfLines, OrdinaryDiffEq, DomainSets
+using ModelingToolkit, MethodOfLines, OrdinaryDiffEq, DomainSets, Revise, Plots
 
 @parameters x t
-@variables c(..) v(..)
+@variables c(..) v(..) fc(..) cv(..)
 Dt = Differential(t)
 Dx = Differential(x)
 Dxx = Differential(x)^2
 
 x_min = t_min = 0
 x_max = 1
-t_max = 10
+t_max = 100
 
-c0(x,t) = 1
-v0(x,t) = 1
+LL = 1/(2*pi)
+Pe = 200
+
+c0(x,t) = sin.(x*2*pi).^2
+v0(x,t) = c0(x,t)
+fc0(x,t) = c0(x,t)/(1.0.+c0(x,t))
+cv0(x,t) = c0(x,t)*v0(x,t)
 
 conc_f(c) = c./(1+c) 
 
 # eq = [Dx(c(x,t)) ~ v(x,t) - Dxx(v(x,t)),
 #       Dt(c(x,t)) ~ Dxx(c(x,t)) - Dx(v(x,t)*c(x,t))]
 
-eq = [Dx(v(x,t)) ~ 2*Dxx(v(x,t)),
-      Dx(c(x,t)) ~ 2*Dxx(c(x,t))]
+eq = [fc(x,t) ~ (c(x,t)/(1+c(x,t))),
+      cv(x,t) ~ v(x,t) * c(x,t),
+      v(x,t) ~ LL^2 * Dxx(v(x,t)) + LL*Dx(fc(x,t)),
+      Dt(c(x,t)) ~ LL*Dxx(c(x,t)) - Pe*Dx(cv(x,t)),
+      ]
+
+# eq = [Dx(v(x,t)) ~ 2*Dxx(v(x,t)),
+#       Dx(c(x,t)) ~ 2*Dxx(c(x,t))]
 
 domains = [x ∈ Interval(x_min, x_max),
            t ∈ Interval(t_min, t_max)]
 
 bcs = [
        c(x,0) ~ c0(x,t),
-       v(x,0) ~ v0(x,t)]
+       v(x,0) ~ v0(x,t),  # it doesn't like it if this line isn't there...
+       c(x_min,t) ~ c(x_max,t),
+       Dx(c(x_min,t)) ~ Dx(c(x_max,t)),
+       v(x_min,t) ~ v(x_max,t),
+       Dx(v(x_min,t)) ~ Dx(v(x_max,t)),
+      #  fc(x,0) ~ fc0(x,t),
+      #  cv(x,0) ~ cv0(x,t)
+      #  v(x_max,t) ~ 0
+       ]
 
-@named pdesys = PDESystem(eq,bcs,domains,[x,t],[c(x,t),v(x,t)])
+@named pdesys = PDESystem(eq,bcs,domains,[x,t],[c(x,t),v(x,t), fc(x,t), cv(x,t)])
 
 N = 100
 
@@ -41,3 +60,17 @@ discretisation = MOLFiniteDifference([x=>N], t, approx_order=order)
 
 println("Discretisation:")
 @time prob = discretize(pdesys,discretisation)
+
+sol = solve(prob,Rosenbrock23())
+
+sol_xs = sol[x]
+sol_ts = sol[t]
+sol_cs = sol[c(x,t)]
+sol_vs = sol[v(x,t)]
+
+anim = @animate for i in 1:length(sol_ts)
+      plot(sol_xs, sol_cs[:,i],ylim=(0,1))
+      plot!(sol_xs, sol_vs[:,i],ylim=(0,1))
+end
+
+gif(anim, "methodoflines.gif",fps=10)
---
+++ scripts/solve_big_eq.jl
@@ -0,0 +1,101 @@
+using DifferentialEquations, Revise
+using 
+include("big_eq2.jl")
+
+# Geometric parameters %%%%%%%%%%%%%%%%%%%%%%%%%
+ri = 1e-3;                     # minimum pole radius to avoid singularity 
+
+# Geometry of the cell
+R0 = 3;                        # radius of the undeformed spherical cell
+vol0 = 2/3*R0^3;               # Volume of the cell
+
+# Geometry of the constraining potential
+a0 = 9/1.1;
+b0 = 6/1.1;
+b = 3.0;         #  The geometry of the constraint is given by
+a = 4.5;         #  z^2/a^2 + r^2/b^2 = 1
+sp = 10;         #  The potential of the constraint is given by
+Vext = 0;        #  V = Vext*exp(sp*d), where d = z^2/a^2 + r^2/b^2 - 1 
+
+#%%%%%%%%% Viscosity and friction %%%%%%%%%%%%%%%%%%%%%%
+mu = 1;                        # viscosity mu = etas
+mub = 0;                       # viscosity mub = (etab-etas)/2
+Ga1 = 0;                       # normal friction coefficient
+Ga2 = 0.1;                    # tangential friction coefficient
+
+#%%%%%%%%% Motor dynamics and active stress %%%%%%%%%%%
+sigeq = 1;                     # Turnover the motors is described by
+ksig = 1;                      # Source term -ksig*(sigma-sigeq)
+xi = 20;                       # Active stress ta = xi*sigma^2/(sigma^2+Sig0^2)
+Sig0 = 1;                      # Hill-function parameter
+Di = 1;                        # Diffusion constant
+
+#%%%%%%%% Elastic properties of the surface %%%%%%%%%%%
+ka = 0.1;                      # Bending rigidity (has to be > 0)
+la = 1;                        # Passive membrane tension
+
+#%%%%%%%% bvp5c parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+RTol = 1e-3;              # relative tolerence 
+Nmax = 50000;             # maximum iteration steps
+dt = 0.01;                # simulation time step
+T = 6;                    # total simulation time
+date = "15-Jan-2024";
+
+
+
+
+### (2) By-hand initial geometry and concentration fields
+### Option 1: Spherical shape, homogeneous concentration
+n_init = 101; # Number of grid points along initial circle
+uc = LinRange(0,1,n_init);
+
+# Initial guess function for spherical surface
+### Defined in functions file ###
+
+# Generate bvpc compatible initial guess data
+fa = xi*sigeq^2/(sigeq^2+ Sig0^2);       # Active stress
+press0 = 2*(la+fa)/R0;                   # Pressure
+# init = guessf(uc,ka,R0,ri,sigeq)
+# init = 6
+init =[uc*pi,
+pi,
+ri.+ R0*sin.(uc*pi),
+R0*(cos.(uc*pi)),
+ka/2*ri*(0.0.-1/R0^2),
+0,
+0,
+0,
+sigeq,
+0,
+2/3*R0^3*(2 .+ cos.(uc*pi)).*sin.(uc*pi/2).^4,
+R0*pi]
+# My understanding is that this doesn't actually solve anything, just
+# puts things in the right structure to be solved 
+# sol = bvpinit(uc,yinit,[pi*R0,press0]);
+#################################################################
+
+# Apply small random perturbations to the motor concentration
+ran = 0.2*rand(1,10).-0.1;
+pert = zeros(size(uc))
+dpert = zeros(size(uc))
+
+# This global is currently a workaround because we're in the global scope from the REPL
+for n in 1:10
+    global pert .+= ran[n]*cos.(n*uc*pi)
+    global dpert .+= - ran[n]*n*pi*sin.(n*uc*pi)
+end
+rr = 0.2*sigeq/maximum(abs.(pert))
+pert .= pert.*rr                 # This is to ensure that the maximum perturbation is no greater than 0.2*sigeq        
+dpert .= dpert.*rr
+init[9] = init[9,:] .+ pert # Add peturbation to concentration
+init[10] = init[10,:] .+ dpert # Add derivative perturbation to concentration derivative
+
+
+#################################################################
+
+sols = []
+
+bvp = BVProblem(shapef!, twobcf!, init, (0,1))
+                                        # ^ this was uc
+
+sol = solve(bvp, Tsit5(), dt=dt)                                        
