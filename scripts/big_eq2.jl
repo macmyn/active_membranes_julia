9108584dbda0aa0f59bebd71634bde5550106516
macmyn <21172144+macmyn@users.noreply.github.com>
2024-08-02 17:44:26 +0100

    bcs in the right order but realisation about size of array

--- scripts/big_eq2.jl
+++ scripts/big_eq2.jl
@@ -1,4 +1,5 @@
 using Revise
+using Infiltrator
 
 function shapef!(dy, y, params, u)
 
@@ -10,20 +11,40 @@ function shapef!(dy, y, params, u)
 # fn0 = para(2); # Pressure
 #dy = zeros(12,1);    # a column vector
 # @infiltrate
-p = y[:,1];    # psi
-dp = y[:,2];
-r = y[:,3];
-z = y[:,4];
-al = y[:,5];
-be = y[:,6];
-vu = y[:,7];
-dvu = y[:,8];
-sig = y[:,9];
-dsig = y[:,10];
+p = @view y[:,1];    # psi
+dp = @view y[:,2];
+r = @view y[:,3];
+z = @view y[:,4];
+al = @view y[:,5];
+be = @view y[:,6];
+vu = @view y[:,7];
+dvu = @view y[:,8];
+sig = @view y[:,9];
+dsig = @view y[:,10];
 #vol = y(11);
-h = y[:,12];
+h = @view y[:,12];
 
 fn0 = params[:press0]
+
+# ka = params[:ka]
+# la = params[:la]
+# Vext = params[:Vext]
+# mu = params[:mu]
+# mub = params[:mub]
+# Ga1 = params[:Ga1]
+# Ga2 = params[:Ga2]
+# sigeq = params[:sigeq]
+# ksig = params[:ksig]
+# xi = params[:ksig]
+# Sig0 = params[:Sig0]
+# Di = params[:Di]
+uc0 = params[:uc0]
+r0c = params[:r0c]
+p0c = params[:p0c]
+z0c = params[:z0c]
+h0c = params[:h0c]
+sig0c = params[:sig0c]
+dt = params[:dt]
 
 if Vext != 0
     # Constraining forces in direction r and z
@@ -43,27 +64,26 @@ end
 # Ga2 = 1./(1.+exp(-dd));
 # Ga2 = exp(dd);
 
-uc0 = params[:uc0]
-r0c = params[:r0c]
-p0c = params[:p0c]
-z0c = params[:z0c]
-h0c = params[:h0c]
-sig0c = params[:sig0c]
-dt = params[:dt]
 
 # Interpolate relevant fields of previous time point solution on points u 
 # requested by bvp5c solver in current step (needed to compute Euler 
 # terms ~(X-X0)/dt in the bvp4c equations
-r0itp = interpolate((uc0,), r0c, Gridded(Linear()))
-r0 = r0itp.(u)  # Interpolate at the points in u;
-z0itp = interpolate((uc0,), z0c, Gridded(Linear()))
-z0 = z0itp.(u)  # Interpolate at the points in u;
-p0itp = interpolate((uc0,), p0c, Gridded(Linear()))
-p0 = p0itp.(u)  # Interpolate at the points in u;
-h0itp = interpolate((uc0,), h0c, Gridded(Linear()))
-h0 = h0itp.(u)  # Interpolate at the points in u;
-sig0itp = interpolate((uc0,), sig0c, Gridded(Linear()))
-sig0 = sig0itp.(u)  # Interpolate at the points in u;
+
+r0 = get_interpolation_over_u(uc0, r0c, u)
+z0 = get_interpolation_over_u(uc0, z0c, u)
+p0 = get_interpolation_over_u(uc0, p0c, u)
+h0 = get_interpolation_over_u(uc0, h0c, u)
+sig0 = get_interpolation_over_u(uc0, sig0c, u)
+# r0itp = interpolate((uc0,), r0c, Gridded(Linear()))
+# r0 = r0itp.(u)  # Interpolate at the points in u;
+# z0itp = interpolate((uc0,), z0c, Gridded(Linear()))
+# z0 = z0itp.(u)  # Interpolate at the points in u;
+# p0itp = interpolate((uc0,), p0c, Gridded(Linear()))
+# p0 = p0itp.(u)  # Interpolate at the points in u;
+# h0itp = interpolate((uc0,), h0c, Gridded(Linear()))
+# h0 = h0itp.(u)  # Interpolate at the points in u;
+# sig0itp = interpolate((uc0,), sig0c, Gridded(Linear()))
+# sig0 = sig0itp.(u)  # Interpolate at the points in u;
 
 # Spontaneous curvature
 c0 = 0;
@@ -98,6 +118,14 @@ dy[:,11].= (1/2).*h.*r.^2.0.*sin.(p);
 
 dy[:,12].= zeros(size(p));
 
+# @infiltrate
+
+end
+
+function get_interpolation_over_u(x_vals, y_vals, u)
+  y_val_interp = interpolate((x_vals,), y_vals, Gridded(Linear()))
+  return y_val_interp.(u) 
+
 end
 
 function guessf(u,ka,R0,ri,sigeq)
@@ -127,12 +155,12 @@ function guessf(u,ka,R0,ri,sigeq)
        ones(size(u))*0.0,
        2/3*R0^3*(2 .+ cos.(u*pi)).*sin.(u*pi/2).^4,
        ones(size(u))*R0*pi]
-  return g
+  return stack(g)
 end
 
 # function twobcf!(ya,yb,para,ka,sp,ri,vol0,Ga2)
-# function twobcf!(res, y, p, x)
 function twobcf!(res, y, p, x)
+# function bc1!(res, y, p)
   # psi = y(1);
   # dpsi = y(2);
   # r = y(3);
@@ -150,8 +178,86 @@ function twobcf!(res, y, p, x)
 #   h = para(1);
   # h = y[:,12]
   h = p[:h0c]  # althought shouldn't h be the line above...?
-  y = y[1]  # for some reason, y is being passed as [y]??
+  # y = y[1]  # for some reason, y is being passed as [y]??
   # @infiltrate
+  ka = p[:ka]
+  ri = p[:ri]
+  Ga2 = p[:Ga2]
+  vol0 = p[:vol0]
+
+  @assert allequal(h)
+  @assert allequal(y[begin][:,2])
+  @infiltrate
+  alpha_i = ka/2*ri*(c0^2-y[begin][:,2][1]l^2/h[1].^2);
+  
+  if Ga2 == 0 # No tangential friction
+    #   beta_z_bc0 = ya(6) .+ yb(6); # beta(0) .+ beta(1) = 0
+    #   beta_z_bc1 = ya(4) .+ yb(4); # z(0) .+ z(1) = 0
+    # beta_z_bc0 = y[:,6][begin] .+ y[:,6][end] #removed this
+    # beta_z_bc1 = y[:,4][begin] .+ y[:,4][end] #andthis
+
+    beta_z_bc0 = y[begin][:,6] .+ y[end][:,6] #removed this
+    beta_z_bc1 = y[begin][:,4] .+ y[end][:,4]
+  else
+    #   beta_z_bc0 = ya(6); # beta(1) = 0
+    #   beta_z_bc1 = yb(6); # beta(1) = 0
+    beta_z_bc0 = y[begin][:,6]
+    beta_z_bc1 = y[end][:,6]
+  end
+
+  # @infiltrate
+
+  res = [ y[begin][:,1][1]-0;   # psi(0) = 0;
+          y[begin][:,3]-ri;      # r(0) = ri;
+          beta_z_bc0;       
+          y[begin][:,5]-alpha_i; # alpha(0)=ka/2*(c0^2-dpsi^2/h^2)...
+          beta_z_bc1;    # 
+          y[begin][:,7]-0;       # vu(0) = 0;
+          y[begin][:,10]-0;      # sigma'(0)=0;
+          y[begin][:,11]-0;      # vol(0) = 0;
+          y[begin][:,12]-h[1];      # h(0) = h0;
+          y[end][:,1]-pi;      # psi(1) = pi;
+          y[end][:,3]-ri;      # r(1) = ri;
+          y[end][:,7]-0;       # vu(1) = 0;
+          y[end][:,10]-0;      # sigma'(1) = 0;
+          y[end][:,11]-vol0];     # vol(1) = vol0;
+  # res[1] = y[:,1][begin]-0;   # psi(0) = 0;
+  # res[2]=        y[:,3][begin]-ri;      # r(0) = ri;
+  # res[3]=        beta_z_bc0;       
+  #   res[4]=      y[:,5][begin]-alpha_i; # alpha(0)=ka/2*(c0^2-dpsi^2/h^2)...
+  #     res[5]=    beta_z_bc1;    # 
+  #       res[6]=  y[:,7][begin]-0;       # vu(0) = 0;
+  #      res[7]=   y[:,10][begin]-0;      # sigma'(0)=0;
+  #      res[8]=   y[:,11][begin]-0;      # vol(0) = 0;
+  #      res[9]=   y[:,12][begin]-h[1];      # h(0) = h0;
+  
+
+end
+
+function bc1!(res, y, p)
+  # psi = y(1);
+  # dpsi = y(2);
+  # r = y(3);
+  # z = y(4);
+  # alpha = y(5);
+  # beta = y(6);
+  # vu = y(7);
+  # dvu = y(8);
+  # sigma = y(9);
+  # dsigma = y(10);
+  # vol = y(11);
+  # h = y(12);
+  # ka = 1;
+  c0 = 0;
+#   h = para(1);
+  # h = y[:,12]
+  h = p[:h0c]  # althought shouldn't h be the line above...?
+  # y = y[1]  # for some reason, y is being passed as [y]??
+  # @infiltrate
+  ka = p[:ka]
+  ri = p[:ri]
+  Ga2 = p[:Ga2]
+  vol0 = p[:vol0]
 
   @assert allequal(h)
   # @infiltrate
@@ -160,8 +266,8 @@ function twobcf!(res, y, p, x)
   if Ga2 == 0 # No tangential friction
     #   beta_z_bc0 = ya(6) .+ yb(6); # beta(0) .+ beta(1) = 0
     #   beta_z_bc1 = ya(4) .+ yb(4); # z(0) .+ z(1) = 0
-    beta_z_bc0 = y[:,6][begin] .+ y[:,6][end]
-    beta_z_bc1 = y[:,4][begin] .+ y[:,4][end]
+    beta_z_bc0 = y[:,6][begin] .+ y[:,6][end] #removed this
+    beta_z_bc1 = y[:,4][begin] .+ y[:,4][end] #andthis
   else
     #   beta_z_bc0 = ya(6); # beta(1) = 0
     #   beta_z_bc1 = yb(6); # beta(1) = 0
@@ -171,20 +277,96 @@ function twobcf!(res, y, p, x)
 
   # @infiltrate
 
-  res = [ y[:,1][begin]-0;   # psi(0) = 0;
-          y[:,3][begin]-ri;      # r(0) = ri;
-          beta_z_bc0;       
-          y[:,5][begin]-alpha_i; # alpha(0)=ka/2*(c0^2-dpsi^2/h^2)...
-          beta_z_bc1;    # 
-          y[:,7][begin]-0;       # vu(0) = 0;
-          y[:,10][begin]-0;      # sigma'(0)=0;
-          y[:,11][begin]-0;      # vol(0) = 0;
-          y[:,12][begin]-h[1];      # h(0) = h0;
-          y[:,1][end]-pi;      # psi(1) = pi;
-          y[:,3][end]-ri;      # r(1) = ri;
-          y[:,7][end]-0;       # vu(1) = 0;
-          y[:,10][end]-0;      # sigma'(1) = 0;
-          y[:,11][end]-vol0];     # vol(1) = vol0;
+  # res = [ y[:,1][begin]-0;   # psi(0) = 0;
+  #         y[:,3][begin]-ri;      # r(0) = ri;
+  #         beta_z_bc0;       
+  #         y[:,5][begin]-alpha_i; # alpha(0)=ka/2*(c0^2-dpsi^2/h^2)...
+  #         beta_z_bc1;    # 
+  #         y[:,7][begin]-0;       # vu(0) = 0;
+  #         y[:,10][begin]-0;      # sigma'(0)=0;
+  #         y[:,11][begin]-0;      # vol(0) = 0;
+  #         y[:,12][begin]-h[1];      # h(0) = h0;
+  #         y[:,1][end]-pi;      # psi(1) = pi;
+  #         y[:,3][end]-ri;      # r(1) = ri;
+  #         y[:,7][end]-0;       # vu(1) = 0;
+  #         y[:,10][end]-0;      # sigma'(1) = 0;
+  #         y[:,11][end]-vol0];     # vol(1) = vol0;
+  res[1] = y[:,1][begin]-0;   # psi(0) = 0;
+  res[2]=        y[:,3][begin]-ri;      # r(0) = ri;
+  res[3]=        beta_z_bc0;       
+    res[4]=      y[:,5][begin]-alpha_i; # alpha(0)=ka/2*(c0^2-dpsi^2/h^2)...
+      res[5]=    beta_z_bc1;    # 
+        res[6]=  y[:,7][begin]-0;       # vu(0) = 0;
+       res[7]=   y[:,10][begin]-0;      # sigma'(0)=0;
+       res[8]=   y[:,11][begin]-0;      # vol(0) = 0;
+       res[9]=   y[:,12][begin]-h[1];      # h(0) = h0;
+  
+
+end
+
+
+  function bc2!(res, y, p)
+    # psi = y(1);
+    # dpsi = y(2);
+    # r = y(3);
+    # z = y(4);
+    # alpha = y(5);
+    # beta = y(6);
+    # vu = y(7);
+    # dvu = y(8);
+    # sigma = y(9);
+    # dsigma = y(10);
+    # vol = y(11);
+    # h = y(12);
+    # ka = 1;
+    c0 = 0;
+  #   h = para(1);
+    # h = y[:,12]
+    h = p[:h0c]  # althought shouldn't h be the line above...?
+    # y = y[1]  # for some reason, y is being passed as [y]??
+    # @infiltrate
+    ri = p[:ri]
+    vol0 = p[:vol0]
+    @assert allequal(h)
+    # @infiltrate
+    # alpha_i = ka/2*ri*(c0^2-y[:,2][begin]^2/h[1].^2);
+    
+    # if Ga2 == 0 # No tangential friction
+    #   #   beta_z_bc0 = ya(6) .+ yb(6); # beta(0) .+ beta(1) = 0
+    #   #   beta_z_bc1 = ya(4) .+ yb(4); # z(0) .+ z(1) = 0
+    #   # beta_z_bc0 = y[:,6][begin] .+ y[:,6][end]
+    #   # beta_z_bc1 = y[:,4][begin] .+ y[:,4][end]
+    # else
+    #   #   beta_z_bc0 = ya(6); # beta(1) = 0
+    #   #   beta_z_bc1 = yb(6); # beta(1) = 0
+    #   beta_z_bc0 = y[:,6][begin]
+    #   beta_z_bc1 = y[:,6][end]
+    # end
+  
+    # @infiltrate
+  
+    # res = [ y[:,1][begin]-0;   # psi(0) = 0;
+    #         y[:,3][begin]-ri;      # r(0) = ri;
+    #         beta_z_bc0;       
+    #         y[:,5][begin]-alpha_i; # alpha(0)=ka/2*(c0^2-dpsi^2/h^2)...
+    #         beta_z_bc1;    # 
+    #         y[:,7][begin]-0;       # vu(0) = 0;
+    #         y[:,10][begin]-0;      # sigma'(0)=0;
+    #         y[:,11][begin]-0;      # vol(0) = 0;
+    #         y[:,12][begin]-h[1];      # h(0) = h0;
+    #         y[:,1][end]-pi;      # psi(1) = pi;
+    #         y[:,3][end]-ri;      # r(1) = ri;
+    #         y[:,7][end]-0;       # vu(1) = 0;
+    #         y[:,10][end]-0;      # sigma'(1) = 0;
+    #         y[:,11][end]-vol0];     # vol(1) = vol0;
+    res[1] = y[:,1][end]-pi;      # psi(1) = pi;
+    res[2]=      y[:,3][end]-ri;      # r(1) = ri;
+    res[3]=      y[:,7][end]-0;       # vu(1) = 0;
+    res[4]=      y[:,10][end]-0;      # sigma'(1) = 0;
+    res[5]=      y[:,11][end]-vol0;
+
+
+
     # res[1] = ya[1]-0;   # psi(0) = 0;
     # res[2] = ya[3]-ri;      # r(0) = ri;
     # res[3] = beta_z_bc0;       
--- scripts/solve_big_eq.jl
+++ scripts/solve_big_eq.jl
@@ -4,39 +4,41 @@ using BoundaryValueDiffEq
 using BenchmarkTools
 using Infiltrator
 using RecursiveArrayTools
+using ODEInterface
+using FileIO,JLD2
 include("big_eq2.jl")
 
 # Geometric parameters %%%%%%%%%%%%%%%%%%%%%%%%%
-ri = 1e-3;                     # minimum pole radius to avoid singularity 
+const ri = 1e-3;                     # minimum pole radius to avoid singularity 
 
 # Geometry of the cell
-R0 = 1;                        # radius of the undeformed spherical cell
-vol0 = 2/3*R0^3;               # Volume of the cell
+const R0 = 1;                        # radius of the undeformed spherical cell
+const vol0 = 2/3*R0^3;               # Volume of the cell
 
 # Geometry of the constraining potential
-a0 = 9/1.1;
-b0 = 6/1.1;
-b = 3.0;         #  The geometry of the constraint is given by
-a = 4.5;         #  z^2/a^2 + r^2/b^2 = 1
-sp = 10;         #  The potential of the constraint is given by
-Vext = 0;        #  V = Vext*exp(sp*d), where d = z^2/a^2 + r^2/b^2 - 1 
+const a0 = 9/1.1;
+const b0 = 6/1.1;
+const b = 3.0;         #  The geometry of the constraint is given by
+const a = 4.5;         #  z^2/a^2 + r^2/b^2 = 1
+const sp = 10;         #  The potential of the constraint is given by
+const Vext = 0;        #  V = Vext*exp(sp*d), where d = z^2/a^2 + r^2/b^2 - 1 
 
 #%%%%%%%%% Viscosity and friction %%%%%%%%%%%%%%%%%%%%%%
-mu = 1;                        # viscosity mu = etas
-mub = 0;                       # viscosity mub = (etab-etas)/2
-Ga1 = 0;                       # normal friction coefficient
-Ga2 = 0.0;                    # tangential friction coefficient
+const mu = 1;                        # viscosity mu = etas
+const mub = 0;                       # viscosity mub = (etab-etas)/2
+const Ga1 = 0;                       # normal friction coefficient
+const Ga2 = 0.0;                    # tangential friction coefficient
 
 #%%%%%%%%% Motor dynamics and active stress %%%%%%%%%%%
-sigeq = 1;                     # Turnover the motors is described by
-ksig = 0;                      # Source term -ksig*(sigma-sigeq)
-xi = 15;                       # Active stress ta = xi*sigma^2/(sigma^2+Sig0^2)
-Sig0 = 1;                      # Hill-function parameter
-Di = 2.7;                        # Diffusion constant
+const sigeq = 1;                     # Turnover the motors is described by
+const ksig = 0;                      # Source term -ksig*(sigma-sigeq)
+const xi = 15;                       # Active stress ta = xi*sigma^2/(sigma^2+Sig0^2)
+const Sig0 = 1;                      # Hill-function parameter
+const Di = 2.7;                        # Diffusion constant
 
 #%%%%%%%% Elastic properties of the surface %%%%%%%%%%%
-ka = 0.01;                      # Bending rigidity (has to be > 0)
-la = 1;                        # Passive membrane tension
+const ka = 0.01;                      # Bending rigidity (has to be > 0)
+const la = 1;                        # Passive membrane tension
 
 #%%%%%%%% bvp5c parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 RTol = 1e-3;              # relative tolerence 
@@ -45,7 +47,7 @@ dt = 0.01;                # simulation time step
 T = 6;                    # total simulation time
 date = "15-Jan-2024";
 
-
+function main()
 
 
 ### (2) By-hand initial geometry and concentration fields
@@ -62,9 +64,8 @@ press0 = 2*(la+fa)/R0;                   # Pressure
 # init = guessf(uc,ka,R0,ri,sigeq)
 # init = 6
 # init =[uc*pi,pi,ri.+ R0*sin.(uc*pi),R0*(cos.(uc*pi)),ka/2*ri*(0.0.-1/R0^2),0,0,0,sigeq,0,2/3*R0^3*(2 .+ cos.(uc*pi)).*sin.(uc*pi/2).^4,R0*pi]
-init = guessf(uc,ka,R0,ri,sigeq)
-init = stack(init)
-init = MArray{Tuple{101,12}}(init)
+guess_init = guessf(uc,ka,R0,ri,sigeq)
+# guess_init = MArray{Tuple{101,12}}(guess_init)
 # init = ArrayPartition(init...)
 # My understanding is that this doesn't actually solve anything, just
 # puts things in the right structure to be solved 
@@ -78,27 +79,243 @@ dpert = zeros(size(uc))
 
 # This global is currently a workaround because we're in the global scope from the REPL
 for n in 1:10
-    global pert .+= ran[n]*cos.(n*uc*pi)
-    global dpert .+= - ran[n]*n*pi*sin.(n*uc*pi)
+    pert .+= ran[n]*cos.(n*uc*pi)
+    dpert .+= - ran[n]*n*pi*sin.(n*uc*pi)
 end
 rr = 0.2*sigeq/maximum(abs.(pert))
-pert .= pert.*rr                 # This is to ensure that the maximum perturbation is no greater than 0.2*sigeq        
-dpert .= dpert.*rr
+# pert .= pert.*rr
+pert = [   -0.1467
+-0.1407
+-0.1234
+-0.0960
+-0.0612
+-0.0217
+ 0.0188
+ 0.0570
+ 0.0896
+ 0.1136
+ 0.1271
+ 0.1287
+ 0.1183
+ 0.0968
+ 0.0657
+ 0.0277
+-0.0141
+-0.0565
+-0.0959
+-0.1292
+-0.1539
+-0.1680
+-0.1704
+-0.1612
+-0.1412
+-0.1119
+-0.0757
+-0.0355
+ 0.0059
+ 0.0454
+ 0.0804
+ 0.1088
+ 0.1289
+ 0.1401
+ 0.1423
+ 0.1364
+ 0.1236
+ 0.1059
+ 0.0854
+ 0.0644
+ 0.0449
+ 0.0289
+ 0.0176
+ 0.0119
+ 0.0119
+ 0.0173
+ 0.0272
+ 0.0402
+ 0.0548
+ 0.0693
+ 0.0819
+ 0.0912
+ 0.0960
+ 0.0957
+ 0.0898
+ 0.0787
+ 0.0628
+ 0.0431
+ 0.0207
+-0.0031
+-0.0270
+-0.0497
+-0.0704
+-0.0881
+-0.1025
+-0.1131
+-0.1201
+-0.1236
+-0.1238
+-0.1213
+-0.1162
+-0.1089
+-0.0995
+-0.0882
+-0.0749
+-0.0596
+-0.0424
+-0.0231
+-0.0021
+ 0.0204
+ 0.0436
+ 0.0669
+ 0.0891
+ 0.1091
+ 0.1255
+ 0.1371
+ 0.1427
+ 0.1414
+ 0.1326
+ 0.1162
+ 0.0925
+ 0.0623
+ 0.0270
+-0.0117
+-0.0517
+-0.0908
+-0.1267
+-0.1572
+-0.1805
+-0.1950
+-0.2000]                 # This is to ensure that the maximum perturbation is no greater than 0.2*sigeq        
+# dpert .= dpert.*rr
+dpert = [         0
+1.1851
+2.2679
+3.1548
+3.7692
+4.0581
+3.9964
+3.5891
+2.8708
+1.9025
+0.7666
+-0.4409
+-1.6183
+-2.6669
+-3.4995
+-4.0477
+-4.2676
+-4.1431
+-3.6866
+-2.9383
+-1.9614
+-0.8370
+0.3434
+1.4858
+2.5016
+3.3151
+3.8695
+4.1310
+4.0913
+3.7672
+3.1982
+2.4429
+1.5724
+0.6644
+-0.2041
+-0.9635
+-1.5574
+-1.9466
+-2.1123
+-2.0567
+-1.8015
+-1.3855
+-0.8600
+-0.2836
+0.2832
+0.7840
+1.1710
+1.4094
+1.4795
+1.3779
+1.1173
+0.7239
+0.2346
+-0.3069
+-0.8546
+-1.3644
+-1.7981
+-2.1264
+-2.3308
+-2.4044
+-2.3515
+-2.1864
+-1.9304
+-1.6094
+-1.2508
+-0.8801
+-0.5187
+-0.1822
+0.1206
+0.3881
+0.6239
+0.8362
+1.0349
+1.2294
+1.4262
+1.6272
+1.8283
+2.0188
+2.1824
+2.2979
+2.3423
+2.2927
+2.1298
+1.8407
+1.4211
+0.8776
+0.2283
+-0.4977
+-1.2614
+-2.0165
+-2.7127
+-3.3001
+-3.7325
+-3.9725
+-3.9942
+-3.7863
+-3.3535
+-2.7167
+-1.9117
+-0.9869
+-0.0000]
 # @infiltrate
-init[:,9] .= init[:,9] .+ pert # Add peturbation to concentration
-init[:,10] .= init[:,10] .+ dpert # Add derivative perturbation to concentration derivative
-
+guess_init[:,9] .= guess_init[:,9] .+ pert # Add peturbation to concentration
+guess_init[:,10] .= guess_init[:,10] .+ dpert # Add derivative perturbation to concentration derivative
+save_object("solve_big_eq_guess_init.jld2",guess_init)
 # @infiltrate
 uc0 = uc
-p0c = init[:,1]
-r0c = init[:,3]
-z0c = init[:,4]
-h0c = init[:,12]
+p0c = guess_init[:,1]
+r0c = guess_init[:,3]
+z0c = guess_init[:,4]
+h0c = guess_init[:,12]
 
 
-sig0c = init[:,9]
+sig0c = guess_init[:,9]
 
-params = Dict(:press0 => press0,
+params = Dict(:ri => ri,
+              :vol0 => vol0,
+              :ka => ka,
+              :la => la,
+              :Vext => Vext,
+              :mu => mu,
+              :mub => mub,
+              :Ga1 => Ga1,
+              :Ga2 => Ga2,
+              :sigeq => sigeq,
+              :ksig => ksig,
+              :xi => xi,
+              :Sig0 => Sig0,
+              :Di => Di,
+              :press0 => press0,
               :uc0 => uc0,
               :p0c => p0c,
               :r0c => r0c,
@@ -114,11 +331,22 @@ params = NamedTuple(p for p in params)
 # @infiltrate
 sols = []
 
-bvp = BVProblem(shapef!, twobcf!, init, (0,1), params)
+bvp = BVProblem(shapef!, twobcf!, guess_init, (0,1), params)
                                         # ^ this was uc
+# @infiltrate
+# bvp = TwoPointBVProblem(shapef!, (bc1!,bc2!), guess_init, (0,1), params; bcresid_prototype = (zeros(9), zeros(5)))
+# bcresid_prototype = (zeros(9), zeros(5)), 
 println("bvp defined")
 # sol = solve(bvp, Shooting(Vern8()), dt=dt)
+# sol = solve(bvp, BVPM2(), dt=dt)
+# sol = solve(bvp, MIRK4(), dt=dt, adaptive=false)
+global dy = similar(guess_init)
+shapef!(dy, guess_init, params, uc0)
+println("run shapef! once")
+println("solving...")
 sol = solve(bvp, MIRK4(), dt=dt, adaptive=false)
-# dy = similar(init)
-# @btime shapef!(dy, init, params, uc0)
-println("finished")                                    
+println("finished")  
+
+end
+
+
