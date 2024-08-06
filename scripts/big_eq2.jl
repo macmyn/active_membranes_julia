479db67eebdeba240a856c0996edbbec9573694a
macmyn <21172144+macmyn@users.noreply.github.com>
2024-08-06 10:58:18 +0100

    WORKING (except for sig and dsig) 🚀

--- scripts/big_eq2.jl
+++ scripts/big_eq2.jl
@@ -128,6 +128,138 @@ function get_interpolation_over_u(x_vals, y_vals, u)
 
 end
 
+
+function shapefsingle!(dy, y, params, u)
+
+  # FUNCTION SETS UP RIGHT-HAND-SIDE EQUATIONS OF ODE SYSTEM SOLVED BY bvp5c
+  # @infiltrate
+  # y = y[1]  # for some reason y (ArrayPartiion) is being passed as Vecor(ArrayPartition) i.e. [y]
+  
+  # Read out current field values
+  # fn0 = para(2); # Pressure
+  #dy = zeros(12,1);    # a column vector
+  # @infiltrate
+  p = y[1];    # psi
+  dp = y[2];
+  r = y[3];
+  z = y[4];
+  al = y[5];
+  be = y[6];
+  vu = y[7];
+  dvu = y[8];
+  sig = y[9];
+  dsig = y[10];
+  #vol = y(11);
+  h = y[12];
+  
+  fn0 = params[:press0]
+  
+  # ka = params[:ka]
+  # la = params[:la]
+  # Vext = params[:Vext]
+  # mu = params[:mu]
+  # mub = params[:mub]
+  # Ga1 = params[:Ga1]
+  # Ga2 = params[:Ga2]
+  # sigeq = params[:sigeq]
+  # ksig = params[:ksig]
+  # xi = params[:ksig]
+  # Sig0 = params[:Sig0]
+  # Di = params[:Di]
+  uc0 = params[:uc0]
+  r0c = params[:r0c]
+  p0c = params[:p0c]
+  z0c = params[:z0c]
+  h0c = params[:h0c]
+  sig0c = params[:sig0c]
+  dt = params[:dt]
+  
+  if Vext != 0
+      # Constraining forces in direction r and z
+      dd = z.^2.0./a.^2 +    r.^2.0./b.^2-1;
+      fr = -2.0.*r.*Sp.*Vext.*exp(Sp.*dd)./b.^2;
+      fz = -2.0.*z.*Sp.*Vext.*exp(Sp.*dd)./a.^2;
+      
+      # External flow-independent forces in normal and tangential direction
+      fn = fn0 +     fz.*cos.(p)+    fr.*sin.(p);
+      fu = -fz.*sin.(p)+    fr.*cos.(p);
+  else
+      # No contraining potential
+      fn = fn0;
+      fu = zeros(size(fn));
+  end
+  
+  # Ga2 = 1./(1.+exp(-dd));
+  # Ga2 = exp(dd);
+  
+  
+  # Interpolate relevant fields of previous time point solution on points u 
+  # requested by bvp5c solver in current step (needed to compute Euler 
+  # terms ~(X-X0)/dt in the bvp4c equations
+  # @infiltrate
+  r0 = get_interpolation_over_u(uc0, r0c, u)
+  z0 = get_interpolation_over_u(uc0, z0c, u)
+  p0 = get_interpolation_over_u(uc0, p0c, u)
+  h0 = get_interpolation_over_u(uc0, h0c, u)
+  sig0 = get_interpolation_over_u(uc0, sig0c, u)
+
+  # r0 = r0c
+  # z0 = z0c
+  # p0 = p0c
+  # h0 = h0c
+  # sig0 = sig0c
+
+  # @infiltrate
+  # r0itp = interpolate((uc0,), r0c, Gridded(Linear()))
+  # r0 = r0itp.(u)  # Interpolate at the points in u;
+  # z0itp = interpolate((uc0,), z0c, Gridded(Linear()))
+  # z0 = z0itp.(u)  # Interpolate at the points in u;
+  # p0itp = interpolate((uc0,), p0c, Gridded(Linear()))
+  # p0 = p0itp.(u)  # Interpolate at the points in u;
+  # h0itp = interpolate((uc0,), h0c, Gridded(Linear()))
+  # h0 = h0itp.(u)  # Interpolate at the points in u;
+  # sig0itp = interpolate((uc0,), sig0c, Gridded(Linear()))
+  # sig0 = sig0itp.(u)  # Interpolate at the points in u;
+  
+  # Spontaneous curvature
+  c0 = 0;
+  
+  ### COPY HERE RIGHT-HAND SIDE EQUATIONS PRODUCED BY MATHEMATICA SCRIPT ### 
+  # println(dy)
+  # println("DP", dp)
+  @infiltrate
+  
+  dy[1]=dp;
+  
+  dy[2]=(-1).*dp.*h.*r.^(-1).*cos.(p).+ be.*h.^2.0.*ka.^(-1).*r.^(-1).*cos.(p).+al.*h.^2.0.*ka.^(-1).*r.^(-1).*sin.(p).+ h.^2.0.*r.^(-2).*cos.(p).*sin.(p) 
+    
+  
+  dy[3]=h.*cos.(p);
+  
+  dy[4]=(-1).*h.*sin.(p);
+  
+  dy[5]=(-1).*c0.*dp.*ka.+ (1/2).*dp.^2.0.*h.^(-1).*ka.+ (1/2).*c0.^2.0.*h.*ka.+ (-1).*fn.*h.*r.*sin.(p).+ 2.0.*dp.*dvu.*h.^(-1).*mu.*r.*sin.(p).+ 2.0.*dp.*dvu.*h.^(-1).*mub.*r.*sin.(p).+ dp.*la.*r.*sig.^2.0.*(sig.^2.0.+ Sig0.^2).^(-1).*sin.(p).+ dp.*la.*r.*Sig0.^2.0.*(sig.^2.0.+ Sig0.^2).^(-1).*sin.(p).+ dp.*r.*sig.^2.0.*(sig.^2.0.+ Sig0.^2).^(-1).*xi.*sin.(p).+ 2.0.*dp.*mub.*vu.*cos.(p).*sin.(p).+ dt.^(-1).*Ga1.*h.*r.*z.*cos.(p).*sin.(p).+ 2.0.*dp.^2.0.*dt.^(-1).*h.^(-1).*mu.*r.*z.*cos.(p).*sin.(p).+ 2.0.*dp.^2.0.*dt.^(-1).*h.^(-1).*mub.*r.*z.*cos.(p).*sin.(p).+ (-1).*dt.^(-1).*Ga1.*h.*r.*z0.*cos.(p).*sin.(p).+ (-2).*dp.^2.0.*dt.^(-1).*h.^(-1).*mu.*r.*z0.*cos.(p).*sin.(p).+ (-2).*dp.^2.0.*dt.^(-1).*h.^(-1).*mub.*r.*z0.*cos.(p).*sin.(p).+ 2.0.*dvu.*mub.*sin.(p).^2.0.+ (-1/2).*h.*ka.*r.^(-2).*sin.(p).^2.0.+ dt.^(-1).*Ga1.*h.*r.^2.0.*sin.(p).^2.0.+ 2.0.*dp.^2.0.*dt.^(-1).*h.^(-1).*mu.*r.^2.0.*sin.(p).^2.0.+ 2.0.*dp.^2.0.*dt.^(-1).*h.^(-1).*mub.*r.^2.0.*sin.(p).^2.0.+ (-1).*dt.^(-1).*Ga1.*h.*r.*r0.*sin.(p).^2.0.+ (-2).*dp.^2.0.*dt.^(-1).*h.^(-1).*mu.*r.*r0.*sin.(p).^2.0.+ (-2).*dp.^2.0.*dt.^(-1).*h.^(-1).*mub.*r.*r0.*sin.(p).^2.0.+ h.*la.*sig.^2.0.*(sig.^2.0.+ Sig0.^2).^(-1).*sin.(p).^2.0.+ h.*la.*Sig0.^2.0.*(sig.^2.0.+ Sig0.^2).^(-1).*sin.(p).^2.0.+ h.*sig.^2.0.*(sig.^2.0.+ Sig0.^2).^(-1).*xi.*sin.(p).^2.0.+ 2.0.*h.*mu.*r.^(-1).*vu.*cos.(p).*sin.(p).^2.0.+ 2.0.*h.*mub.*r.^(-1).*vu.*cos.(p).*sin.(p).^2.0.+ 4.0*dp.*dt.^(-1).*mub.*z.*cos.(p).*sin.(p).^2.0.+ (-4).*dp.*dt.^(-1).*mub.*z0.*cos.(p).*sin.(p).^2.0.+ 4.0*dp.*dt.^(-1).*mub.*r.*sin.(p).^3.0.+ (-4).*dp.*dt.^(-1).*mub.*r0.*sin.(p).^3.0.+ 2.0.*dt.^(-1).*h.*mu.*r.^(-1).*z.*cos.(p).*sin.(p).^3.0.+ 2.0.*dt.^(-1).*h.*mub.*r.^(-1).*z.*cos.(p).*sin.(p).^3.0.+ (-2).*dt.^(-1).*h.*mu.*r.^(-1).*z0.*cos.(p).*sin.(p).^3.0.+ (-2).*dt.^(-1).*h.*mub.*r.^(-1).*z0.*cos.(p).*sin.(p).^3.0.+ 2.0.*dt.^(-1).*h.*mu.*sin.(p).^4.0.+ 2.0.*dt.^(-1).*h.*mub.*sin.(p).^4.0.+ (-2).*dt.^(-1).*h.*mu.*r.^(-1).*r0.*sin.(p).^4.0.+ (-2).*dt.^(-1).*h.*mub.*r.^(-1).*r0.*sin.(p).^4;
+  
+  dy[6]=(-1).*fn.*h.*r.*cos.(p).+ 2.0.*dp.*dvu.*h.^(-1).*mu.*r.*cos.(p).+ 2.0.*dp.*dvu.*h.^(-1).*mub.*r.*cos.(p).+ dp.*la.*r.*sig.^2.0.*(sig.^2.0.+ Sig0.^2).^(-1).*cos.(p).+ dp.*la.*r.*Sig0.^2.0.*(sig.^2.0.+ Sig0.^2).^(-1).*cos.(p).+ dp.*r.*sig.^2.0.*(sig.^2.0.+ Sig0.^2).^(-1).*xi.*cos.(p).+ 2.0.*dp.*mub.*vu.*cos.(p).^2.0.+ dt.^(-1).*Ga1.*h.*r.*z.*cos.(p).^2.0.+ 2.0.*dp.^2.0.*dt.^(-1).*h.^(-1).*mu.*r.*z.*cos.(p).^2.0.+ 2.0.*dp.^2.0.*dt.^(-1).*h.^(-1).*mub.*r.*z.*cos.(p).^2.0.+ (-1).*dt.^(-1).*Ga1.*h.*r.*z0.*cos.(p).^2.0.+ (-2).*dp.^2.0.*dt.^(-1).*h.^(-1).*mu.*r.*z0.*cos.(p).^2.0.+ (-2).*dp.^2.0.*dt.^(-1).*h.^(-1).*mub.*r.*z0.*cos.(p).^2.0.+ 2.0.*dvu.*mub.*cos.(p).*sin.(p).+ dt.^(-1).*Ga1.*h.*r.^2.0.*cos.(p).*sin.(p).+ 2.0.*dp.^2.0.*dt.^(-1).*h.^(-1).*mu.*r.^2.0.*cos.(p).*sin.(p).+ 2.0.*dp.^2.0.*dt.^(-1).*h.^(-1).*mub.*r.^2.0.*cos.(p).*sin.(p).+ (-1).*dt.^(-1).*Ga1.*h.*r.*r0.*cos.(p).*sin.(p).+ (-2).*dp.^2.0.*dt.^(-1).*h.^(-1).*mu.*r.*r0.*cos.(p).*sin.(p).+ (-2).*dp.^2.0.*dt.^(-1).*h.^(-1).*mub.*r.*r0.*cos.(p).*sin.(p).+ h.*la.*sig.^2.0.*(sig.^2.0.+ Sig0.^2).^(-1).*cos.(p).*sin.(p).+ h.*la.*Sig0.^2.0.*(sig.^2.0.+ Sig0.^2).^(-1).*cos.(p).*sin.(p).+ h.*sig.^2.0.*(sig.^2.0.+ Sig0.^2).^(-1).*xi.*cos.(p).*sin.(p).+ 2.0.*h.*mu.*r.^(-1).*vu.*cos.(p).^2.0.*sin.(p).+ 2.0.*h.*mub.*r.^(-1).*vu.*cos.(p).^2.0.*sin.(p).+ 4.0*dp.*dt.^(-1).*mub.*z.*cos.(p).^2.0.*sin.(p).+ (-4).*dp.*dt.^(-1).*mub.*z0.*cos.(p).^2.0.*sin.(p).+ 4.0*dp.*dt.^(-1).*mub.*r.*cos.(p).*sin.(p).^2.0.+ (-4).*dp.*dt.^(-1).*mub.*r0.*cos.(p).*sin.(p).^2.0.+ 2.0.*dt.^(-1).*h.*mu.*r.^(-1).*z.*cos.(p).^2.0.*sin.(p).^2.0.+ 2.0.*dt.^(-1).*h.*mub.*r.^(-1).*z.*cos.(p).^2.0.*sin.(p).^2.0.+ (-2).*dt.^(-1).*h.*mu.*r.^(-1).*z0.*cos.(p).^2.0.*sin.(p).^2.0.+ (-2).*dt.^(-1).*h.*mub.*r.^(-1).*z0.*cos.(p).^2.0.*sin.(p).^2.0.+ 2.0.*dt.^(-1).*h.*mu.*cos.(p).*sin.(p).^3.0.+ 2.0.*dt.^(-1).*h.*mub.*cos.(p).*sin.(p).^3.0.+ (-2).*dt.^(-1).*h.*mu.*r.^(-1).*r0.*cos.(p).*sin.(p).^3.0.+ (-2).*dt.^(-1).*h.*mub.*r.^(-1).*r0.*cos.(p).*sin.(p).^3;
+  
+  dy[7]=dvu;
+  
+  dy[8]=(-1/2).*fu.*h.^2.0.*(mu.+ mub).^(-1).+ (1/2).*Ga2.*h.^2.0.*(mu.+ mub).^(-1).*vu.+ dsig.*h.*(mu.+ mub).^(-1).*sig.^3.0.*(sig.^2.0.+ Sig0.^2).^(-2).*xi.+ (-1).*dsig.*h.*(mu.+ mub).^(-1).*sig.*(sig.^2.0.+ Sig0.^2).^(-1).*xi.+ (-1) .*dvu.*h.*mu.*(mu.+ mub).^(-1).*r.^(-1).*cos.(p).+ (-1).*dvu.*h.*mub.*( mu.+ mub).^(-1).*r.^(-1).*cos.(p).+ (-1).*dp.^2.0.*dt.^(-1).*mu.*(mu.+ mub) .^(-1).*r.*cos.(p).+ (-1).*dp.^2.0.*dt.^(-1).*mub.*(mu.+ mub).^(-1).*r.* cos.(p).+ dp.^2.0.*dt.^(-1).*mu.*(mu.+ mub).^(-1).*r0.*cos.(p).+ dp.^2.0.* dt.^(-1).*mub.*(mu.+ mub).^(-1).*r0.*cos.(p).+ h.^2.0.*mu.*(mu.+ mub).^(-1).*r.^(-2).*vu.*cos.(p).^2.0.+ h.^2.0.*mub.*(mu.+ mub).^(-1).*r.^(-2).*vu.* cos.(p).^2.0.+ (-1).*be.*dt.^(-1).*h.^2.0.*ka.^(-1).*mu.*(mu.+ mub).^(-1).* r.^(-1).*z.*cos.(p).^2.0.+ dp.*dt.^(-1).*h.*mub.*(mu.+ mub).^(-1).*r.^( -1).*z.*cos.(p).^2.0.+ (-1).*be.*dt.^(-1).*h.^2.0.*ka.^(-1).*mub.*(mu.+  mub).^(-1).*r.^(-1).*z.*cos.(p).^2.0.+ be.*dt.^(-1).*h.^2.0.*ka.^(-1).* mu.*(mu.+ mub).^(-1).*r.^(-1).*z0.*cos.(p).^2.0.+ (-1).*dp.*dt.^(-1).*h.* mub.*(mu.+ mub).^(-1).*r.^(-1).*z0.*cos.(p).^2.0.+ be.*dt.^(-1).*h.^2.0.* ka.^(-1).*mub.*(mu.+ mub).^(-1).*r.^(-1).*z0.*cos.(p).^2.0.+ (-1).*dp.* dt.^(-1).*h.*mub.*(mu.+ mub).^(-1).*r.^(-1).*z.*cos.(2.0.*p).+ dp.*dt.^( -1).*h.*mub.*(mu.+ mub).^(-1).*r.^(-1).*z0.*cos.(2.0.*p).+ dp.*h.*mub.*( mu.+ mub).^(-1).*r.^(-1).*vu.*sin.(p).+ dp.^2.0.*dt.^(-1).*mu.*(mu.+ mub) .^(-1).*z.*sin.(p).+ dp.^2.0.*dt.^(-1).*mub.*(mu.+ mub).^(-1).*z.*sin.(p).+  (-1).*dp.^2.0.*dt.^(-1).*mu.*(mu.+ mub).^(-1).*z0.*sin.(p).+ (-1).* dp.^2.0.*dt.^(-1).*mub.*(mu.+ mub).^(-1).*z0.*sin.(p).+ dp.*dt.^(-1).*h.* mu.*(mu.+ mub).^(-1).*cos.(p).*sin.(p).+ (-1).*be.*dt.^(-1).*h.^2.0.*ka.^( -1).*mu.*(mu.+ mub).^(-1).*cos.(p).*sin.(p).+ dp.*dt.^(-1).*h.*mub.*(mu.+  mub).^(-1).*cos.(p).*sin.(p).+ (-1).*be.*dt.^(-1).*h.^2.0.*ka.^(-1).* mub.*(mu.+ mub).^(-1).*cos.(p).*sin.(p).+ (-1).*dp.*dt.^(-1).*h.*mu.*( mu.+ mub).^(-1).*r.^(-1).*r0.*cos.(p).*sin.(p).+ be.*dt.^(-1).*h.^2.0.* ka.^(-1).*mu.*(mu.+ mub).^(-1).*r.^(-1).*r0.*cos.(p).*sin.(p).+ (-1).* dp.*dt.^(-1).*h.*mub.*(mu.+ mub).^(-1).*r.^(-1).*r0.*cos.(p).*sin.(p).+  be.*dt.^(-1).*h.^2.0.*ka.^(-1).*mub.*(mu.+ mub).^(-1).*r.^(-1).*r0.* cos.(p).*sin.(p).+ (-1).*al.*dt.^(-1).*h.^2.0.*ka.^(-1).*mu.*(mu.+ mub).^( -1).*r.^(-1).*z.*cos.(p).*sin.(p).+ (-1).*al.*dt.^(-1).*h.^2.0.*ka.^(-1).*mub.*(mu.+ mub).^(-1).*r.^(-1).*z.*cos.(p).*sin.(p).+ al.*dt.^(-1).* h.^2.0.*ka.^(-1).*mu.*(mu.+ mub).^(-1).*r.^(-1).*z0.*cos.(p).*sin.(p).+  al.*dt.^(-1).*h.^2.0.*ka.^(-1).*mub.*(mu.+ mub).^(-1).*r.^(-1).*z0.* cos.(p).*sin.(p).+ dt.^(-1).*h.^2.0.*mu.*(mu.+ mub).^(-1).*r.^(-2).*z.* cos.(p).^2.0.*sin.(p).+ dt.^(-1).*h.^2.0.*mub.*(mu.+ mub).^(-1).*r.^(-2).* z.*cos.(p).^2.0.*sin.(p).+ (-1).*al.*dt.^(-1).*h.^2.0.*ka.^(-1).*mu.*(mu.+  mub).^(-1).*sin.(p).^2.0.+ (-1).*al.*dt.^(-1).*h.^2.0.*ka.^(-1).*mub.*( mu.+ mub).^(-1).*sin.(p).^2.0.+ al.*dt.^(-1).*h.^2.0.*ka.^(-1).*mu.*(mu.+  mub).^(-1).*r.^(-1).*r0.*sin.(p).^2.0.+ al.*dt.^(-1).*h.^2.0.*ka.^(-1).* mub.*(mu.+ mub).^(-1).*r.^(-1).*r0.*sin.(p).^2.0.+ (-1).*dt.^(-1).*h.^2.0.* mu.*(mu.+ mub).^(-1).*r.^(-1).*cos.(p).*sin.(p).^2.0.+ (-1).*dt.^(-1).* h.^2.0.*mub.*(mu.+ mub).^(-1).*r.^(-1).*cos.(p).*sin.(p).^2.0.+ (-1/2).*dp.* dt.^(-1).*h.*mu.*(mu.+ mub).^(-1).*sin.(2.0.*p).+ (-1).*dp.*dt.^(-1).*h.* mub.*(mu.+ mub).^(-1).*sin.(2.0.*p).+ (1/2).*dp.*dt.^(-1).*h.*mu.*(mu.+  mub).^(-1).*r.^(-1).*r0.*sin.(2.0.*p).+ dp.*dt.^(-1).*h.*mub.*(mu.+ mub) .^(-1).*r.^(-1).*r0.*sin.(2.0.*p).+ (-1/2).*dt.^(-1).*h.^2.0.*mu.*(mu.+  mub).^(-1).*r.^(-2).*z.*cos.(p).*sin.(2.0.*p).+ (-1/2).*dt.^(-1).*h.^2.0.* mub.*(mu.+ mub).^(-1).*r.^(-2).*z.*cos.(p).*sin.(2.0.*p).+ (1/2).*dt.^(-1).*h.^2.0.*mu.*(mu.+ mub).^(-1).*r.^(-1).*sin.(p).*sin.(2.0.*p).+ (1/2).* dt.^(-1).*h.^2.0.*mub.*(mu.+ mub).^(-1).*r.^(-1).*sin.(p).*sin.(2.0.*p).+  dp.*dt.^(-1).*h0.*mu.*(mu.+ mub).^(-1).*sin.(p.+ (-1).*p0).+ dp.*dt.^(-1).*h0.*mub.*(mu.+ mub).^(-1).*sin.(p.+ (-1).*p0).+ dt.^(-1).*h.*h0.*mub.*( mu.+ mub).^(-1).*r.^(-1).*sin.(p).*sin.(p.+ (-1).*p0);
+  
+  dy[9]= dsig;
+  
+  dy[10] = Di.^(-1).*dvu.*h.*sig.+ Di.^(-1).*dt.^(-1).*h.^2.0.*sig.+ Di.^(-1).* h.^2.0.*ksig.*sig.+ (-1).*Di.^(-1).*dt.^(-1).*h.^2.0.*sig0.+ (-1).*Di.^( -1).*h.^2.0.*ksig.*sigeq.+ Di.^(-1).*dsig.*h.*vu.+ (-1).*dsig.*h.*r.^( -1).*cos.(p).+ (-1).*Di.^(-1).*dsig.*dt.^(-1).*h.*r.*cos.(p).+ Di.^(-1) .*dsig.*dt.^(-1).*h.*r0.*cos.(p).+ Di.^(-1).*h.^2.0.*r.^(-1).*sig.*vu.* cos.(p).+ Di.^(-1).*dp.*dt.^(-1).*h.*sig.*z.*cos.(p).+ (-1).*Di.^(-1).* dp.*dt.^(-1).*h.*sig.*z0.*cos.(p).+ Di.^(-1).*dp.*dt.^(-1).*h.*r.* sig.*sin.(p).+ (-1).*Di.^(-1).*dp.*dt.^(-1).*h.*r0.*sig.*sin.(p).+ Di.^( -1).*dsig.*dt.^(-1).*h.*z.*sin.(p).+ (-1).*Di.^(-1).*dsig.*dt.^(-1).* h.*z0.*sin.(p).+ Di.^(-1).*dt.^(-1).*h.^2.0.*r.^(-1).*sig.*z.*cos.(p).* sin.(p).+ (-1).*Di.^(-1).*dt.^(-1).*h.^2.0.*r.^(-1).*sig.*z0.*cos.(p).* sin.(p).+ Di.^(-1).*dt.^(-1).*h.^2.0.*sig.*sin.(p).^2.0.+ (-1).*Di.^(-1).* dt.^(-1).*h.^2.0.*r.^(-1).*r0.*sig.*sin.(p).^2;
+  
+  dy[11]= (1/2).*h.*r.^2.0.*sin.(p);
+  
+  dy[12]= 0#zeros(size(p));
+  
+  @infiltrate
+  
+  end
+  
+
+
 function guessf(u,ka,R0,ri,sigeq)
   # psi = y(1);
   # dpsi = y(2);
@@ -155,6 +287,73 @@ function guessf(u,ka,R0,ri,sigeq)
        ones(size(u))*0.0,
        2/3*R0^3*(2 .+ cos.(u*pi)).*sin.(u*pi/2).^4,
        ones(size(u))*R0*pi]
+  return stack(g)
+end
+
+function initialGuess(p,t)
+  # psi = y(1);
+  # dpsi = y(2);
+  # r = y(3);
+  # z = y(4);
+  # alpha = y(5);
+  # beta = y(6);
+  # vu = y(7);
+  # dvu = y(8);
+  # sigma = y(9);
+  # dsigma = y(10);
+  # vol =y(11);
+  # h = y(12);
+  # ka = 1;
+  #ka = 1;
+
+  ka = p[:ka]
+  R0 = p[:R0]
+  ri = p[:ri]
+  sigeq = p[:sigeq]
+
+  g = [t*pi,
+       pi,
+       ri.+ R0*sin.(t*pi),
+       R0*(cos.(t*pi)),
+       ka/2*ri*(0.0.-1/R0^2),
+       0.0,
+       0.0,
+       0.0,
+       sigeq,
+       0.0,
+       2/3*R0^3*(2 .+ cos.(t*pi)).*sin.(t*pi/2).^4,
+       R0*pi]
+  return stack(g)
+end
+
+
+function guessfsingle(u,ka,R0,ri,sigeq)
+  # psi = y(1);
+  # dpsi = y(2);
+  # r = y(3);
+  # z = y(4);
+  # alpha = y(5);
+  # beta = y(6);
+  # vu = y(7);
+  # dvu = y(8);
+  # sigma = y(9);
+  # dsigma = y(10);
+  # vol =y(11);
+  # h = y(12);
+  # ka = 1;
+  #ka = 1;
+  g = [0,
+       pi,
+       ri.+ R0*0,
+       R0*1,
+       ka/2*ri*(0.0.-1/R0^2),
+       0.0,
+       0.0,
+       0.0,
+       sigeq,
+       0.0,
+       2/3*R0^3*2, #(2 .+ cos.(u*pi)).*sin.(u*pi/2).^4,
+       R0*pi]
   return stack(g)
 end
 
@@ -187,7 +386,7 @@ function twobcf!(res, y, p, x)
 
   @assert allequal(h)
   @assert allequal(y[begin][:,2])
-  @infiltrate
+  # @infiltrate
   alpha_i = ka/2*ri*(c0^2-y[begin][:,2][1]l^2/h[1].^2);
   
   if Ga2 == 0 # No tangential friction
@@ -233,6 +432,86 @@ function twobcf!(res, y, p, x)
   
 
 end
+
+
+
+function twobcfsingle!(res, y, p, x)
+  # function bc1!(res, y, p)
+    # psi = y(1);
+    # dpsi = y(2);
+    # r = y(3);
+    # z = y(4);
+    # alpha = y(5);
+    # beta = y(6);
+    # vu = y(7);
+    # dvu = y(8);
+    # sigma = y(9);
+    # dsigma = y(10);
+    # vol = y(11);
+    # h = y(12);
+    # ka = 1;
+    c0 = 0;
+  #   h = para(1);
+    # h = y[12]
+    h = p[:h]
+    # h = p[:h0c]  # althought shouldn't h be the line above...?
+    # y = y[1]  # for some reason, y is being passed as [y]??
+    # @infiltrate
+    ka = p[:ka]
+    ri = p[:ri]
+    Ga2 = p[:Ga2]
+    vol0 = p[:vol0]
+  
+    # @assert allequal(h)
+    # @assert allequal(y[begin][:,2])
+    @infiltrate
+    alpha_i = ka/2*ri*(c0^2-y[begin][2]^2/h.^2);
+    
+    if Ga2 == 0 # No tangential friction
+      #   beta_z_bc0 = ya(6) .+ yb(6); # beta(0) .+ beta(1) = 0
+      #   beta_z_bc1 = ya(4) .+ yb(4); # z(0) .+ z(1) = 0
+      # beta_z_bc0 = y[:,6][begin] .+ y[:,6][end] #removed this
+      # beta_z_bc1 = y[:,4][begin] .+ y[:,4][end] #andthis
+  
+      beta_z_bc0 = y[begin][6] .+ y[end][6] #removed this
+      beta_z_bc1 = y[begin][4] .+ y[end][4]
+    else
+      #   beta_z_bc0 = ya(6); # beta(1) = 0
+      #   beta_z_bc1 = yb(6); # beta(1) = 0
+      beta_z_bc0 = y[begin][6]
+      beta_z_bc1 = y[end][6]
+    end
+  
+    # @infiltrate
+  
+    res = [ y[begin][1]-0;   # psi(0) = 0;
+            y[begin][3]-ri;      # r(0) = ri;
+            beta_z_bc0;       
+            y[begin][5]-alpha_i; # alpha(0)=ka/2*(c0^2-dpsi^2/h^2)...
+            beta_z_bc1;    # 
+            y[begin][7]-0;       # vu(0) = 0;
+            y[begin][10]-0;      # sigma'(0)=0;
+            y[begin][11]-0;      # vol(0) = 0;
+            y[begin][12]-h[1];      # h(0) = h0;
+            y[end][1]-pi;      # psi(1) = pi;
+            y[end][3]-ri;      # r(1) = ri;
+            y[end][7]-0;       # vu(1) = 0;
+            y[end][10]-0;      # sigma'(1) = 0;
+            y[end][11]-vol0];     # vol(1) = vol0;
+    # res[1] = y[:,1][begin]-0;   # psi(0) = 0;
+    # res[2]=        y[:,3][begin]-ri;      # r(0) = ri;
+    # res[3]=        beta_z_bc0;       
+    #   res[4]=      y[:,5][begin]-alpha_i; # alpha(0)=ka/2*(c0^2-dpsi^2/h^2)...
+    #     res[5]=    beta_z_bc1;    # 
+    #       res[6]=  y[:,7][begin]-0;       # vu(0) = 0;
+    #      res[7]=   y[:,10][begin]-0;      # sigma'(0)=0;
+    #      res[8]=   y[:,11][begin]-0;      # vol(0) = 0;
+    #      res[9]=   y[:,12][begin]-h[1];      # h(0) = h0;
+    
+  
+  end
+  
+
 
 function bc1!(res, y, p)
   # psi = y(1);
--- scripts/solve_big_eq.jl
+++ scripts/solve_big_eq.jl
@@ -7,6 +7,7 @@ using RecursiveArrayTools
 using ODEInterface
 using FileIO,JLD2
 include("big_eq2.jl")
+include("jac.jl")
 
 # Geometric parameters %%%%%%%%%%%%%%%%%%%%%%%%%
 const ri = 1e-3;                     # minimum pole radius to avoid singularity 
@@ -43,7 +44,7 @@ const la = 1;                        # Passive membrane tension
 #%%%%%%%% bvp5c parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 RTol = 1e-3;              # relative tolerence 
 Nmax = 50000;             # maximum iteration steps
-dt = 0.01;                # simulation time step
+dt = 0.001;                # simulation time step
 T = 6;                    # total simulation time
 date = "15-Jan-2024";
 
@@ -65,6 +66,7 @@ press0 = 2*(la+fa)/R0;                   # Pressure
 # init = 6
 # init =[uc*pi,pi,ri.+ R0*sin.(uc*pi),R0*(cos.(uc*pi)),ka/2*ri*(0.0.-1/R0^2),0,0,0,sigeq,0,2/3*R0^3*(2 .+ cos.(uc*pi)).*sin.(uc*pi/2).^4,R0*pi]
 guess_init = guessf(uc,ka,R0,ri,sigeq)
+guess_init_single = guessfsingle(uc,ka,R0,ri,sigeq)
 # guess_init = MArray{Tuple{101,12}}(guess_init)
 # init = ArrayPartition(init...)
 # My understanding is that this doesn't actually solve anything, just
@@ -83,210 +85,9 @@ for n in 1:10
     dpert .+= - ran[n]*n*pi*sin.(n*uc*pi)
 end
 rr = 0.2*sigeq/maximum(abs.(pert))
-# pert .= pert.*rr
-pert = [   -0.1467
--0.1407
--0.1234
--0.0960
--0.0612
--0.0217
- 0.0188
- 0.0570
- 0.0896
- 0.1136
- 0.1271
- 0.1287
- 0.1183
- 0.0968
- 0.0657
- 0.0277
--0.0141
--0.0565
--0.0959
--0.1292
--0.1539
--0.1680
--0.1704
--0.1612
--0.1412
--0.1119
--0.0757
--0.0355
- 0.0059
- 0.0454
- 0.0804
- 0.1088
- 0.1289
- 0.1401
- 0.1423
- 0.1364
- 0.1236
- 0.1059
- 0.0854
- 0.0644
- 0.0449
- 0.0289
- 0.0176
- 0.0119
- 0.0119
- 0.0173
- 0.0272
- 0.0402
- 0.0548
- 0.0693
- 0.0819
- 0.0912
- 0.0960
- 0.0957
- 0.0898
- 0.0787
- 0.0628
- 0.0431
- 0.0207
--0.0031
--0.0270
--0.0497
--0.0704
--0.0881
--0.1025
--0.1131
--0.1201
--0.1236
--0.1238
--0.1213
--0.1162
--0.1089
--0.0995
--0.0882
--0.0749
--0.0596
--0.0424
--0.0231
--0.0021
- 0.0204
- 0.0436
- 0.0669
- 0.0891
- 0.1091
- 0.1255
- 0.1371
- 0.1427
- 0.1414
- 0.1326
- 0.1162
- 0.0925
- 0.0623
- 0.0270
--0.0117
--0.0517
--0.0908
--0.1267
--0.1572
--0.1805
--0.1950
--0.2000]                 # This is to ensure that the maximum perturbation is no greater than 0.2*sigeq        
-# dpert .= dpert.*rr
-dpert = [         0
-1.1851
-2.2679
-3.1548
-3.7692
-4.0581
-3.9964
-3.5891
-2.8708
-1.9025
-0.7666
--0.4409
--1.6183
--2.6669
--3.4995
--4.0477
--4.2676
--4.1431
--3.6866
--2.9383
--1.9614
--0.8370
-0.3434
-1.4858
-2.5016
-3.3151
-3.8695
-4.1310
-4.0913
-3.7672
-3.1982
-2.4429
-1.5724
-0.6644
--0.2041
--0.9635
--1.5574
--1.9466
--2.1123
--2.0567
--1.8015
--1.3855
--0.8600
--0.2836
-0.2832
-0.7840
-1.1710
-1.4094
-1.4795
-1.3779
-1.1173
-0.7239
-0.2346
--0.3069
--0.8546
--1.3644
--1.7981
--2.1264
--2.3308
--2.4044
--2.3515
--2.1864
--1.9304
--1.6094
--1.2508
--0.8801
--0.5187
--0.1822
-0.1206
-0.3881
-0.6239
-0.8362
-1.0349
-1.2294
-1.4262
-1.6272
-1.8283
-2.0188
-2.1824
-2.2979
-2.3423
-2.2927
-2.1298
-1.8407
-1.4211
-0.8776
-0.2283
--0.4977
--1.2614
--2.0165
--2.7127
--3.3001
--3.7325
--3.9725
--3.9942
--3.7863
--3.3535
--2.7167
--1.9117
--0.9869
--0.0000]
+pert .= pert.*rr
+                 # This is to ensure that the maximum perturbation is no greater than 0.2*sigeq        
+dpert .= dpert.*rr
 # @infiltrate
 guess_init[:,9] .= guess_init[:,9] .+ pert # Add peturbation to concentration
 guess_init[:,10] .= guess_init[:,10] .+ dpert # Add derivative perturbation to concentration derivative
@@ -301,7 +102,8 @@ h0c = guess_init[:,12]
 
 sig0c = guess_init[:,9]
 
-params = Dict(:ri => ri,
+params = Dict(:R0 => R0,
+              :ri => ri,
               :vol0 => vol0,
               :ka => ka,
               :la => la,
@@ -315,6 +117,7 @@ params = Dict(:ri => ri,
               :xi => xi,
               :Sig0 => Sig0,
               :Di => Di,
+              :h => pi*R0,
               :press0 => press0,
               :uc0 => uc0,
               :p0c => p0c,
@@ -330,22 +133,28 @@ params = NamedTuple(p for p in params)
 #################################################################
 # @infiltrate
 sols = []
-
-bvp = BVProblem(shapef!, twobcf!, guess_init[1,:], (0,1), params)
+# bvpf = BVPFunction(shapefsingle!,jacsingle)
+# bvp = BVProblem(shapefsingle!, twobcfsingle!, guess_init_single, (0,1), params)
                                         # ^ this was uc
-# @infiltrate
+bvp = BVProblem(shapefsingle!, twobcfsingle!, initialGuess, (0,1), params)
+@infiltrate
 # bvp = TwoPointBVProblem(shapef!, (bc1!,bc2!), guess_init, (0,1), params; bcresid_prototype = (zeros(9), zeros(5)))
 # bcresid_prototype = (zeros(9), zeros(5)), 
 println("bvp defined")
 # sol = solve(bvp, Shooting(Vern8()), dt=dt)
 # sol = solve(bvp, BVPM2(), dt=dt)
 # sol = solve(bvp, MIRK4(), dt=dt, adaptive=false)
-global dy = similar(guess_init)
-shapef!(dy, guess_init, params, uc0)
-println("run shapef! once")
+# global dy = similar(guess_init)
+# shapef!(dy, guess_init, params, uc0)
+# println("run shapef! once")
 println("solving...")
-sol = solve(bvp, MIRK4(), dt=dt, adaptive=false)
+sol = solve(bvp, MIRK4(), dt=dt, adaptive=false)#,abstol=1e-10,reltol=1e-10)
+# sol = solve(bvp, Shooting(AutoTsit5(Rosenbrock23())),dt=dt,adaptive=false)
+# sol = solve(bvp, Shooting(Vern8()), dt=dt, adaptive=false)
+@infiltrate
 println("finished")  
+
+return sol
 
 end
 
